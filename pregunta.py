0c3bd0b6878ab5e777ce9c2059334f1dc43c16b1
alosanchezvi <59671162+alosanchezvi@users.noreply.github.com>
2023-04-13 16:18:34 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -16,6 +16,32 @@ def ingest_data():
 
     #
     # Inserte su código aquí
+    path=r'clusters_report.txt'
+    with open(path, 'r') as f:
+        content = f.read()
+    lineas = content.split('\n')
+
+    indices_numeros=[]
+    for i,elemento in enumerate(lineas):
+        if any(caracter.isdigit() for caracter in elemento):
+            indices_numeros.append(i)
+
+    lista_concat=[]    
+    for a,b in zip(indices_numeros, indices_numeros[1:]):
+        lista_concat.append(' '.join(lineas[a:b]))
+
+    contenido=[]
+    for i in (lista_concat):
+        contenido.append(re.findall(pattern, i))
+    contenido001=[list(cont[0]) for cont in  contenido]
+    for cont in range(len(contenido001)):
+        contenido001[cont][3]=re.sub('\s+', ' ', contenido001[cont][3]).strip()
+        columnas=lineas[:2]
+    nombre_columnas=columnas[0].split('  ')[:2]+columnas[0].split('  ')[3:-2]
+    nombre_columnas[1]=nombre_columnas[1]+' palabras clave'
+    nombre_columnas[2]=nombre_columnas[2]+' palabras clave'
+    nombre_columnas=[i.lower().strip().replace(' ','_') for i in nombre_columnas]
+    df=pd.DataFrame(contenido001,columns=nombre_columnas)
     #
 
     return df
